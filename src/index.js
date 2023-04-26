67c3bdbd435b5de7cc36f933e99c2eb911bfb3df
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-04-26 10:45:04 +0300

    12345

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import axios from 'axios';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
